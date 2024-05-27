0f24ae9786a451f557cd130eac3d724b2293f9b5
Shyam Mardiya <164022489+ShyamMardiya8@users.noreply.github.com>
2024-05-27 07:34:13 -0700

    Add files via upload

---
+++ src/assets/components/Button.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function button() {
+  return (
+    <>
+    <div className='Board-row'>
+        <button className='box'></button>
+        <button className='box'></button>
+        <button className='box'></button>
+    </div>
+    </>
+  );
+}
